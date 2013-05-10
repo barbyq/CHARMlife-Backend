c799907bfb1ae9c182a4bc4d97cc382dc76e2c15
jcarlos7121 <jchinojob7@gmail.com>
2013-05-10 13:39:00 -0500

    hiding stuff jejejeje

---
+++ dbc/articulosDAO.php
@@ -0,0 +1,40 @@
+<?php 
+
+/**
+* 
+*/
+class articulosDAO
+{
+	private $dbc;
+
+	function __construct($con)
+	{
+		$this->dbc = $con;
+	}
+
+	public function getArticulos()
+	{
+		return 0;
+	}
+
+	public function getArticulo($id)
+	{
+		return 0;
+	}
+
+	public function insertArticulo($articulo)
+	{
+		
+	}
+
+	public function editArticulo($articulo)
+	{
+		
+	}
+
+	public function deleteArticulo()
+	{
+		
+	}
+}
+ ?>
