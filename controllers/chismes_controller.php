c2b941d3a88ce9d3092d823105beb08f1a4e0877
jcarlos7121 <jchinojob7@gmail.com>
2013-05-23 12:45:37 -0500

    Chismes

--- controllers/chismes_controller.php
+++ controllers/chismes_controller.php
@@ -10,5 +10,109 @@ $dbconnect = new dbconnect('charm_charmlifec536978');
 $dbc = $dbconnect->getConnection();
 $chismidao = new chismesDAO($dbc);
 
-echo json_encode($chismidao->getChismes());
+if (isset($_POST['receiver']) && $_POST['receiver'] == "registro") {
+	print_r($_REQUEST);
+	$chisme = new stdClass;
+	$chisme->titulo = $_POST['titulo'];
+	$chisme->texto = $_POST['texto'];
+	$chisme->link = $_POST['link'];
+
+	if (strlen($_POST['fecha']) == 0) {
+		$chisme->fecha = date("Y")."-".date("m")."-".date("d");
+	}else{
+		$fechaexplotada = explode("/",$_POST['fecha']);
+		$chisme->fecha = $fechaexplotada[2]."-".$fechaexplotada[0]."-".$fechaexplotada[1];
+	}
+	$chisme->foto = "0";
+	
+	$idgenerado = $chismidao->insertChisme($chisme);
+	$temporaldir = $_POST['temporal'];
+	
+	if (is_dir('../TemporalChismes/'.$temporaldir)) {
+		mkdir('../Chismes/'.$idgenerado);
+		recurse_copy('../TemporalChismes/'.$temporaldir,'../Chismes/'.$idgenerado);
+		rmdir_recurse('../TemporalChismes/'.$temporaldir);
+		$escaneo = scandir('../Chismes/'.$idgenerado);
+		$tipo = substr($escaneo[2],strlen($escaneo[2]) - 3);
+		print_r($tipo);
+		rename('../Chismes/'.$idgenerado.'/'.$escaneo[2],'../Chismes/'.$idgenerado.'/'.$idgenerado.'.'.$tipo);
+		$chisme->foto = 'Chismes/'.$idgenerado.'/'.$idgenerado.'.'.$tipo;
+		$chisme->id = $idgenerado;
+		print_r($chisme);
+		$chismidao->updateChisme($chisme);
+	}	
+
+}elseif (isset($_POST['receiver']) && $_POST['receiver'] == "imagenchisme") {
+	$generado = $_POST['generado'];
+	print_r($_FILES['imagenchisme']["type"]);
+	if (is_dir('../TemporalChismes/'.$generado)) {
+		rmdir_recurse('../TemporalChismes/'.$generado);
+		mkdir('../TemporalChismes/'.$generado);
+		if ($_FILES['imagenchisme']['type'] =='image/jpeg') {
+			print_r("Es jpg");
+			move_uploaded_file($_FILES['imagenchisme']['tmp_name'],'../TemporalChismes/'.$generado.'/'.$generado.'.jpg');
+		}else{
+			move_uploaded_file($_FILES['imagenchisme']['tmp_name'],'../TemporalChismes/'.$generado.'/'.$generado.'.png');
+		}		
+	}else{
+		mkdir('../TemporalChismes/'.$generado);
+		if ($_FILES['imagenchisme']['type'] =='image/jpeg') {
+			print_r("Es jpg");
+			move_uploaded_file($_FILES['imagenchisme']['tmp_name'],'../TemporalChismes/'.$generado.'/'.$generado.'.jpg');
+		}else{
+			move_uploaded_file($_FILES['imagenchisme']['tmp_name'],'../TemporalChismes/'.$generado.'/'.$generado.'.png');
+		}
+	}
+}elseif(isset($_POST['receiver']) && $_POST['receiver'] == "damechisme"){
+	$id = $_POST['chismeid'];
+	$chisme = $chismidao->getChismeById($id);
+
+	$fechaexplotada = explode("-",$chisme->fecha);
+	$chisme->year = $fechaexplotada[0];
+	$chisme->mes = $fechaexplotada[1];
+	$chisme->dia = $fechaexplotada[2];
+	echo json_encode($chisme);
+}elseif (isset($_POST['receiver']) && $_POST['receiver'] == "update") {
+	print_r($_REQUEST);
+	$chisme = new stdClass;
+	$chisme->titulo = $_POST['titulo'];
+	$chisme->texto = $_POST['texto'];
+	$chisme->link = $_POST['link'];
+	$chisme->id = $_POST['chismeid'];
+	if (strlen($_POST['fecha']) == 0) {
+		$chisme->fecha = date("Y")."-".date("m")."-".date("d");
+	}else{
+		$fechaexplotada = explode("/",$_POST['fecha']);
+		$chisme->fecha = $fechaexplotada[2]."-".$fechaexplotada[0]."-".$fechaexplotada[1];
+	}
+	$chisme->foto = "Chismes/".$chisme->id."/".$chisme->id.".jpg";
+	$chismidao->updateChisme($chisme);
+}elseif (isset($_POST['receiver']) &&  $_POST['receiver'] == "updateimagen") {
+	$idamodificar = $_POST['idchisme'];
+	if (is_dir("../Chismes/".$idamodificar)) {
+		rmdir_recurse("../Chismes/".$idamodificar);
+		mkdir("../Chismes/".$idamodificar);
+		if ($_FILES['imagenchisme']['type'] == 'image/jpeg') {
+			move_uploaded_file($_FILES['imagenchisme']['tmp_name'],"../Chismes/".$idamodificar."/".$idamodificar.".jpg");
+		}elseif ($_FILES['imagenchisme']['type'] == 'image/png') {
+			move_uploaded_file($_FILES['imagenchisme']['tmp_name'],"../Chismes/".$idamodificar."/".$idamodificar.".png");
+		}
+	}else{
+		mkdir("../Chismes/".$idamodificar);
+		if ($_FILES['imagenchisme']['type'] == 'image/jpeg') {
+			move_uploaded_file($_FILES['imagenchisme']['tmp_name'],"../Chismes/".$idamodificar."/".$idamodificar.".jpg");
+		}elseif ($_FILES['imagenchisme']['type'] == 'image/png') {
+			move_uploaded_file($_FILES['imagenchisme']['tmp_name'],"../Chismes/".$idamodificar."/".$idamodificar.".png");
+		}
+	}
+}elseif (isset($_POST['receiver']) && $_POST['receiver'] == "borrar") {
+	$id = $_POST['idchisme'];
+	$chismidao->deleteChisme($id);
+	if (is_dir("../Chismes/".$id)) {
+		rmdir_recurse("../Chismes/".$id);
+	}
+}
+else{
+	echo json_encode($chismidao->getChismes());
+}
 ?>
--- js/viewsChismes.js
+++ js/viewsChismes.js
@@ -1,7 +1,19 @@
 var ViewRowChismes = Backbone.View.extend({	
 	tagName:"tr",
+	events:{
+		"click .borrar":"borrar"
+	},borrar:function(e) {
+		var contexto = this;
+		var elemento = $(e.currentTarget);
+		var id = elemento.attr('id');
+		if (confirm("Seguro que quieres borrarlo?")) {
+			$.post("controllers/chismes_controller.php",{receiver:"borrar",idchisme:this.model.get("id")},function  (response) {
+				contexto.$el.remove();
+			});
+		};
+	},
 	render:function  () {
-		var contenido = "<td>"+this.model.get("id")+"</td><td>"+this.model.get("titulo")+"</td><td>"+this.model.get("fecha")+"</td><td>"+this.model.get("link")+"</td><td><a href='#chisme/"+this.model.get("id")+"/edit'>Modificar</a></td><td><a id="+this.model.get("id")+" class='.borrar'>Borrar</a></td>";
+		var contenido = "<td>"+this.model.get("id")+"</td><td>"+this.model.get("titulo")+"</td><td>"+this.model.get("fecha")+"</td><td>"+this.model.get("link")+"</td><td><a href='#chisme/"+this.model.get("id")+"/edit'>Modificar</a></td><td><a id="+this.model.get("id")+" class='borrar'>Borrar</a></td>";
 		this.$el.html(contenido);
 		return this;
 	}
@@ -25,6 +37,93 @@ var ViewTablaChismes = Backbone.View.extend({
 	},
 	render:function() {
 	  this.addAll();
+	  return this;
+	}
+});
+
+var RegisterChismeView =Backbone.View.extend({
+	id:"registrochisme",
+	className:"colab_chisme",
+	events:{
+		"click .registrarchisme":"registrarchisme"
+	},
+	initialize:function  () {
+	  this.iditemporalizador = generaidtexto();
+	},registrarchisme:function  () {
+	  var valores = $('#registrochisme :input').serializeArray();
+	  valores.push({name:"temporal",value:this.iditemporalizador});
+	  valores.push({name:"receiver",value:"registro"});
+	  $.post("controllers/chismes_controller.php",valores,function  (response) {
+	    alert("Guardado con exito!");
+	    CharmRouter.navigate("chismes",{trigger:true});
+	  });
+	},
+	loader:function() {	  
+		var contexto = this;
+		$('#fechachisme').datepicker();
+	  	var desubidita = $('#dominicdococo').upload({
+			name: 'imagenchisme',
+            action: 'controllers/chismes_controller.php',
+            enctype: 'multipart/form-data',
+            autoSubmit: true,
+            params:{receiver:'imagenchisme',generado:contexto.iditemporalizador},
+			onComplete:function() {
+				var nombre = desubidita.filename();
+				var tipo = nombre.substr(nombre.length - 3);
+				$('#imagenchisme').attr('src','TemporalChismes/'+contexto.iditemporalizador+'/'+contexto.iditemporalizador+"."+tipo);
+			}
+		});
+	},
+	render:function  () {
+		var contexto = this;
+		$.get("templates/RegisterChisme.handlebars",function  (response) {
+		  contexto.$el.html(response);
+		});
+	  return this;
+	}
+});
+
+var ChismeEditView = Backbone.View.extend({
+	id:"updatechisme",
+	className:"colab_chisme",
+	events:{
+		"click .updatechisme":"updatechisme"
+	},updatechisme:function  () {
+		var valores = $('#updatechisme :input').serializeArray();
+		valores.push({name:"receiver",value:"update"});
+		valores.push({name:"chismeid",value:this.model.get("id")});
+		$.post("controllers/chismes_controller.php",valores,function  (response) {
+			alert("Modificado con exito!");
+			CharmRouter.navigate("chismes",{trigger:true});
+		});
+		console.log(valores);
+	},
+	loader:function  () {
+		var contexto = this;
+		$('#fechachismedit').datepicker();
+		if (this.model.get("foto") != 0) {
+			$('#imagenchismeedit').attr("src",this.model.get("foto")+"?timestamp=" + new Date().getTime());
+		};
+	  	var desubidita = $('#dominicdococoedit').upload({
+			name: 'imagenchisme',
+            action: 'controllers/chismes_controller.php',
+            enctype: 'multipart/form-data',
+            autoSubmit: true,
+            params:{receiver:'updateimagen',idchisme:contexto.model.get("id")},
+			onComplete:function() {
+				var nombre = desubidita.filename();
+				var tipo = nombre.substr(nombre.length - 3);
+				$('#imagenchismeedit').attr('src','Chismes/'+contexto.model.get("id")+'/'+contexto.model.get("id")+"."+tipo+"?timestamp=" + new Date().getTime());
+			}
+		});
+	},
+	render:function  () {
+		var contexto = this;
+		$.get("templates/UpdateChisme.handlebars",function  (response) {
+			var con = Handlebars.compile(response);
+			var eichtiemel = con(contexto.model.toJSON());
+		  	contexto.$el.html(eichtiemel);
+		});
 	  return this;
 	}
 });
